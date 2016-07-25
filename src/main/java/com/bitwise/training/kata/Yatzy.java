bec530b3a3cbd6ad186c19dacac46ea8391557c5
omk <omkomawar222@gmail.com>
2016-07-25 21:39:42 +0530

    Kata Update 1

--- src/main/java/com/bitwise/training/kata/Yatzy.java
+++ src/main/java/com/bitwise/training/kata/Yatzy.java
@@ -9,20 +9,99 @@ import java.util.List;
  */
 public class Yatzy {
 
+    int score;
 
     public void validate(Integer... dice) {
         if (dice.length !=5){
             throw new InvalidDicesException();
         }
-        
+
         for (int diceSide: dice) {
-            if (diceSide > 6)
-                throw new InvalidDicesException();
+        if (diceSide > 6 )
+            throw new InvalidDicesException();
+        }
+    }
+
+    public int chance(int... dice) {
+        int chanceScore = 0;
+        for (int diceSide: dice) {
+            chanceScore += diceSide;
         }
 
-
-
+        return chanceScore;
     }
+
+    public int yatzy(int... dice) {
+
+        int first = dice[0];
+        for(int i = 1; i < dice.length ; i++) {
+            if (dice[i] != first) return 0;
+        }
+        return 50;
+    }
+
+    public int ones(int... dice) {
+        int onesScore = 0;
+        for (int diceSide: dice) {
+            if (diceSide == 1){
+                onesScore++;
+            }
+        }
+        return onesScore;
+    }
+
+    public int twos(int... dice) {
+        int twosScore = 0;
+        for (int diceSide: dice) {
+            if (diceSide == 2){
+                twosScore += 2;
+            }
+        }
+        return twosScore;
+    }
+
+    public int threes(int... dice) {
+        int threesScore = 0;
+        for (int diceSide: dice) {
+            if (diceSide == 3){
+                threesScore += 3;
+            }
+        }
+        return threesScore;
+    }
+
+    public int fours(int... dice) {
+        int foursScore = 0;
+        for (int diceSide: dice) {
+            if (diceSide == 4){
+                foursScore += 4;
+            }
+        }
+        return foursScore;
+    }
+    public int fives(int... dice) {
+        int fivesScore = 0;
+        for (int diceSide: dice) {
+            if (diceSide == 5){
+                fivesScore += 5;
+            }
+        }
+        return fivesScore;
+    }
+    public int sixs(int... dice) {
+        int fivesScore = 0;
+        for (int diceSide: dice) {
+            if (diceSide == 6){
+                fivesScore += 6;
+            }
+        }
+        return fivesScore;
+    }
+
+    public int pair(int... dice) {
+        return 0;
+    }
+
 
     public class InvalidDicesException extends RuntimeException {
     }
--- src/test/java/com/bitwise/training/kata/YatzyTests.java
+++ src/test/java/com/bitwise/training/kata/YatzyTests.java
@@ -1,5 +1,6 @@
 package com.bitwise.training.kata;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -8,7 +9,7 @@ import org.junit.Test;
 public class YatzyTests {
 
     @Test(expected = Yatzy.InvalidDicesException.class)
-    public void itShouldValidateRollingDiceSide_number_of_dice(){
+    public void itShouldValidateRollingDiceSide_numberOfDice(){
         //given
         Yatzy yatzy = new Yatzy();
         //when
@@ -16,19 +17,141 @@ public class YatzyTests {
         //then
     }
     @Test(expected = Yatzy.InvalidDicesException.class)
-    public void itShouldValidateRollingDiceSide_sideOfDice(){
+    public void itShouldValidateRollingDiceSide_checkValidDice(){
         //given
         Yatzy yatzy = new Yatzy();
         //when
         yatzy.validate(1,2,3,21,5);
         //then
     }
-    @Test(expected = Yatzy.InvalidDicesException.class)
-    public void itShouldValidateRollingDiceSide_(){
+
+    @Test
+    public void itShouldPerformChanceRule(){
         //given
         Yatzy yatzy = new Yatzy();
         //when
-        yatzy.validate(1,2,3,21,5);
+        int actual = yatzy.chance(1,1,3,3,6);
+        int expected = 14;
+        int actual2 = yatzy.chance(4,5,5,6,1);
+        int expected2 = 21;
+
         //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
     }
+
+    @Test
+    public void itShouldPerformYatzyRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.yatzy(1,1,1,1,1);
+        int expected = 50;
+        int actual2 = yatzy.yatzy(1,1,1,2,1);
+        int expected2 = 0;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void itShouldPerformOnesRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.ones(1,1,1,1,1);
+        int expected = 5;
+        int actual2 = yatzy.ones(1,2,2,3,1);
+        int expected2 = 2;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void itShouldPerformTwosRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.twos(2,2,1,1,1);
+        int expected = 4;
+        int actual2 = yatzy.twos(1,2,2,3,1);
+        int expected2 = 4;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void itShouldPerformThreesRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.threes(2,3,1,3,1);
+        int expected = 6;
+        int actual2 = yatzy.threes(1,2,3,3,1);
+        int expected2 = 6;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void itShouldPerformFoursRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.fours(4,4,1,1,1);
+        int expected = 8;
+        int actual2 = yatzy.fours(3,3,3,3,1);
+        int expected2 = 0;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void itShouldPerformFivesRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.fives(2,5,5,5,1);
+        int expected = 15;
+        int actual2 = yatzy.fives(1,2,3,3,1);
+        int expected2 = 0;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+    @Test
+    public void itShouldPerformSixsRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.sixs(6,6,6,6,1);
+        int expected = 24;
+        int actual2 = yatzy.sixs(1,2,3,3,1);
+        int expected2 = 0;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    public void itShouldPerformPairRule(){
+        //given
+        Yatzy yatzy = new Yatzy();
+        //when
+        int actual = yatzy.pair(3,3,3,4,4);
+        int expected = 8;
+        int actual2 = yatzy.pair(1,1,6,2,6);
+        int expected2 = 12;
+        int actual3 = yatzy.pair(3,3,3,4,1);
+        int expected3 = 6;
+        int actual4 = yatzy.pair(3,3,3,3,1);
+        int expected4 = 6;
+        //then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected3, actual3);
+        Assert.assertEquals(expected4, actual4);
+    }
+
+
+
 }
